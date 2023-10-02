be5654afe93ac3939ec5b15c68b394a5d45f585d
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-10-02 16:25:32 -0500

    intiial commit

---
+++ HCK1-DBP/src/main/java/com/example/hck1dbp/Hck1DbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.hck1dbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hck1DbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hck1DbpApplication.class, args);
+    }
+
+}
