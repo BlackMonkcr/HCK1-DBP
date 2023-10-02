86fcaf9b98527f336f322836ebbd637da6bef245
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-10-02 16:55:38 -0500

    Entidades Alumno&Grade

--- HCK1-DBP/src/main/java/com/example/hck1dbp/domain/Alumno.java
+++ HCK1-DBP/src/main/java/com/example/hck1dbp/domain/Alumno.java
@@ -1,4 +1,60 @@
 package com.example.hck1dbp.domain;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "alumnos")
 public class Alumno {
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "alumno_id", nullable = false, unique = true)
+    private Long id;
+
+    private String LastName;
+
+    private String code;
+
+    @OneToMany(mappedBy = "alumno")
+    private List<Grade> grades;
+
+    /* ---------------------------------------------------------------------------------------- */
+
+    // Constructors
+    public Alumno() {
+    }
+
+    public Alumno(String LastName, String code, List<Grade> grades) {
+        this.grades = grades;
+        this.LastName = LastName;
+        this.code = code;
+    }
+
+    /* ---------------------------------------------------------------------------------------- */
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public List<Grade> getGrades() {
+        return grades;
+    }
+
+    /* ---------------------------------------------------------------------------------------- */
+
+    // Setters
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public void setGrades(List<Grade> grades) {
+        this.grades = grades;
+    }
 }
--- HCK1-DBP/src/main/java/com/example/hck1dbp/domain/Grade.java
+++ HCK1-DBP/src/main/java/com/example/hck1dbp/domain/Grade.java
@@ -1,4 +1,61 @@
 package com.example.hck1dbp.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "grades")
 public class Grade {
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "grade_id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "score", nullable = false)
+    private Long score;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "alumno_id", referencedColumnName = "alumno_id")
+    private Alumno alumno;
+
+    /* ---------------------------------------------------------------------------------------- */
+
+    // Constructors
+    public Grade() {
+    }
+
+    public Grade(Long score, Alumno alumno) {
+        this.score = score;
+        this.alumno = alumno;
+    }
+
+    /* ---------------------------------------------------------------------------------------- */
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    /* ---------------------------------------------------------------------------------------- */
+
+    // Setters
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    /* ---------------------------------------------------------------------------------------- */
 }
